2f09b1ccec930dc8d8c3cf21e3805a6e86fe9643
Yuutzu <j.polinar.540369@umindanao.edu.ph>
2026-02-12 17:43:34 +0800

    Remove Hotel folder from repository

--- Hotel/admin/config/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$dbuser = "root";
-$dbpass = "";
-$host = "localhost:3307";
-$db = "hotel";
-$mysqli = new mysqli($host, $dbuser, $dbpass, $db);
--- Hotel/client/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_destroy();
-header('Location: index.php'); // Redirect to homepage
-unset($_SESSION['otp']);
-unset($_SESSION['otp_expiry']);
-exit();
